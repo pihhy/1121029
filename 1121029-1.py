354dea8869791d3eb384c1733b332460a498b215
pihhy <ljlandhhy@gmail.com>
2023-10-29 14:19:47 +0800

    modify

--- 1121029-1.py
+++ 1121029-1.py
@@ -1,1 +1,12 @@
-print("hello")
+from gpiozero import LED
+from time import sleep
+
+led = LED(27)
+# led = LED("GPIO27")   # 給 GPIO 腳位，跟上一行意義相同！
+# led = LED("J8:13")    # 定義為樹莓派的腳位
+
+while True:
+  led.on()
+  sleep(1)
+  led.off()
+  sleep(1)
