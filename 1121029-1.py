ea981bb8befeeeb956240953f564a9ae9b278e32
pihhy <ljlandhhy@gmail.com>
2023-10-29 16:14:30 +0800

    apupdate

--- 1121029-1.py
+++ 1121029-1.py
@@ -5,8 +5,12 @@ led = LED(27)
 # led = LED("GPIO27")   # 給 GPIO 腳位，跟上一行意義相同！
 # led = LED("J8:13")    # 定義為樹莓派的腳位
 
-while True:
+i=1
+while i<=100:
+  print(i)
   led.on()
   sleep(1)
   led.off()
   sleep(1)
+  i=i+1
+  
